bbead6b9421033cff6f95f8776d141d419513472
kouki ito <ko1u4ki0i3to2u7@gmail.com>
2025-01-26 14:18:33 +0900

    20250126_バリデーション修正

--- src/app/Http/Requests/SellRequest.php
+++ src/app/Http/Requests/SellRequest.php
@@ -28,7 +28,7 @@ class SellRequest extends FormRequest
             'categories' => ['required'],
             'condition' => 'required',
             'item_name' => 'required',
-            'item_detail' => 'required',
+            'item_detail' => 'required|max:255',
             'item_price' => 'required|min:0',
         ];
     }
@@ -41,6 +41,7 @@ class SellRequest extends FormRequest
             'condition.required' => '状態を選択してください',
             'item_name.required' => '商品名を入力してください',
             'item_detail.required' => '商品の説明を入力してください',
+            'item_detail.max' => '文字数を255文字以内にしてください',
             'item_price.required' => '販売価格を入力してください',
             'item_price.min' => '販売価格を0円以上にしてください',
         ];
